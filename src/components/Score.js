33b04216e9c8c36397068a41c403d3e0c0b1898a
Indravathi Botcha <81157199+Indhu248@users.noreply.github.com>
2025-02-20 14:14:45 +0530

    Update Score.js

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,24 +1,15 @@
-import { openDB } from 'idb';
+// import { openDB } from 'idb';
 import { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Confetti from 'react-confetti';
 import award from '../utils/award.png'
+import { integerTypeQuestions, multipleChoiceQuestions } from '../utils/questions';
+
 
 const Score = () => {
   const location = useLocation();
   const {score} = location.state || {score: 0};
-  useEffect(() => {
-    const saveScore = async () => {
-      const db = await openDB('quiz-db', 1, {
-        upgrade(db) {
-          db.createObjectStore('scores', { autoIncrement: true });
-        },
-      });
-      await db.add('scores', { score, date: new Date() });
-    };
-
-    saveScore();
-  }, [score]);
+  const length = integerTypeQuestions.length + multipleChoiceQuestions.length
 
   return (
     <div className='score__board'>
@@ -26,11 +17,9 @@ const Score = () => {
        <div className='your__score'>
        <img src={award} alt='award'/>
        <h1> Congratulations</h1>
-       <h2>Your Score: {score}</h2>
+       <h2>Your Score: {score} outof {length}</h2>
        <p>Wanna retake the test <Link to='/home'>Click here</Link></p>
+       {/* <p>See your previous history <Link to='/history'>Click here</Link></p> */}
        </div>
+      {/* Additional code to display past scores */}
     </div>
-  );
-};
-
-export default Score;
